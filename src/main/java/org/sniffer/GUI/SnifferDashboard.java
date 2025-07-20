024f715b82dc98fc552439aca768e781ac84e50c
leolegge <llegge1@sheffield.ac.uk>
2025-07-20 15:52:38 +0100

    Set up JFrame initial class

---
+++ src/main/java/org/sniffer/GUI/SnifferDashboard.java
@@ -0,0 +1,6 @@
+package org.sniffer.GUI;
+
+import javax.swing.*;
+
+public class SnifferDashboard extends JFrame {
+}
--- src/main/java/org/sniffer/Main.java
+++ src/main/java/org/sniffer/Main.java
@@ -1,7 +1,14 @@
 package org.sniffer;
+import org.sniffer.GUI.*;
+
 
 public class Main {
     public static void main(String[] args) {
+
+
+
+
+
         System.out.println("Hello, World!");
     }
 }

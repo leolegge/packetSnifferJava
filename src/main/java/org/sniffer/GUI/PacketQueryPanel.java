4fbb5d6ef50c71035f36b035aa157a6ab18a8399
leolegge <llegge1@sheffield.ac.uk>
2025-07-21 15:22:12 +0100

    Added clear and add filter buttons

--- src/main/java/org/sniffer/GUI/PacketQueryPanel.java
+++ src/main/java/org/sniffer/GUI/PacketQueryPanel.java
@@ -14,13 +14,21 @@ import java.awt.event.ActionListener;
 public class PacketQueryPanel extends JPanel {
 
     JTextField textField = new JTextField();
+    JPanel filterButtonPanel = new JPanel(new BorderLayout());
     JButton applyFilterButton = new JButton("Apply filter");
+    JButton clearFilterButton = new JButton("Clear filter");
 
     public PacketQueryPanel() {
         this.setPreferredSize(new Dimension(0, 20));
         this.setLayout(new BorderLayout());
         this.add(textField, BorderLayout.CENTER);
-        this.add(applyFilterButton, BorderLayout.EAST);
+
+
+        this.add(filterButtonPanel, BorderLayout.EAST);
+
+
+        filterButtonPanel.add(applyFilterButton, BorderLayout.WEST);
+        filterButtonPanel.add(clearFilterButton, BorderLayout.EAST);
 
         textField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -29,6 +37,12 @@ public class PacketQueryPanel extends JPanel {
         });
 
         applyFilterButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        clearFilterButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
             }

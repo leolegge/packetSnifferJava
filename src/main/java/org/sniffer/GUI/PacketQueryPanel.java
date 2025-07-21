f5c4524378d6807ca16e70895235e76afef86300
leolegge <llegge1@sheffield.ac.uk>
2025-07-21 15:07:12 +0100

    Made some changes to the query panel

--- src/main/java/org/sniffer/GUI/PacketQueryPanel.java
+++ src/main/java/org/sniffer/GUI/PacketQueryPanel.java
@@ -4,6 +4,8 @@ package org.sniffer.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This is the panel to hold the query line in which users can enter a query
@@ -11,8 +13,28 @@ import java.awt.*;
  */
 public class PacketQueryPanel extends JPanel {
 
+    JTextField textField = new JTextField();
+    JButton applyFilterButton = new JButton("Apply filter");
+
     public PacketQueryPanel() {
         this.setPreferredSize(new Dimension(0, 20));
-        this.setBackground(Color.GREEN);
+        this.setLayout(new BorderLayout());
+        this.add(textField, BorderLayout.CENTER);
+        this.add(applyFilterButton, BorderLayout.EAST);
+
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        applyFilterButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+
     }
 }

aa09840b618a4be4aa029a3ac6a4326a85fe23f9
leolegge <llegge1@sheffield.ac.uk>
2025-07-22 16:28:54 +0100

    Added listener to table

--- src/main/java/org/sniffer/GUI/packetPanels/PacketsDisplayPanel.java
+++ src/main/java/org/sniffer/GUI/packetPanels/PacketsDisplayPanel.java
@@ -24,9 +24,31 @@ public class PacketsDisplayPanel extends JPanel {
         mainPacketTable = new DefaultTableModel(PACKET_COLUMNS_NAMES, 0);
         table = new JTable(mainPacketTable);
 
+        mainPacketTable.addRow(new String[] {"test","test2", "test3", "test4", "test5"});
+
+        ListSelectionModel mainPacketTableSelectionModel = table.getSelectionModel();
+
+
+        /////////////////////////////////////////////THIS IS AN EXAMPLE OF HOW TO GET DATA FROM TABLE
+
+        mainPacketTableSelectionModel.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    System.out.println("Selected Row: " + selectedRow);
+                    //TODO can make a object from this or push to the PacketInfomationPanel
+                    for (int i = 0; i < PACKET_COLUMNS_NAMES.length; i++) {
+                        System.out.println(table.getValueAt(selectedRow, i));
+                    }
+                }
+            }
+        });
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
         packetsDisplayScrollPanel = new PacketsDisplayScrollPanel(table);
-
-
 
         this.add(packetsDisplayScrollPanel, BorderLayout.NORTH);
 
